91658c560562c36b3bd3c931f717c811541dbf34
anthonydfsa <91759793+anthonydfsa@users.noreply.github.com>
2021-10-15 16:47:53 -0400

    TEST
    TEST

---
+++ test.py
@@ -0,0 +1,25 @@
+# numbers, strings, lists, booleans, conditionals, loops
+a = 10.90 # float
+b = 12 # int
+sum = a + b
+print(type(a)) # float
+
+saying_hi = "hi my name is echo"
+print(saying_hi * 4)
+
+age_list = [12, 15, 34, 38]
+print(age_list[0])
+
+is_it_raining = False
+print(is_it_raining) # true
+
+if is_it_raining:
+    print("It is raining!")
+else:
+    print("It is NOT raining")
+
+for i in range(5):
+    print(str(i) + ": hi")
+
+
+# change to show again
